197b804709d18fb1b5436fec9e91d8809f0ab2cb
Unknown <sato.ryo.so0@is.naist.jp>
2018-12-27 16:29:24 +0900

    Check seg_image topic

--- deeplab/deeplab_ros.py
+++ deeplab/deeplab_ros.py
@@ -28,7 +28,7 @@ class DeepLabModel(object):
     """Creates and loads pretrained deeplab model."""
     self.config = tf.ConfigProto(
                 gpu_options = tf.GPUOptions(
-                    per_process_gpu_memory_fraction=0.4,
+                    per_process_gpu_memory_fraction=1,
                     visible_device_list="0",
                     allow_growth=True
             )
@@ -95,58 +95,30 @@ def vis_segmentation(image, seg_map):
   cv_image      = np.asarray(image)
   cv_seg_image  = np.asarray(seg_image)
   cv_mask_image = cv2.addWeighted(cv_seg_image, 0.9, cv_image, 0.8, 0)
+  # cv_mask_image2 = cv2.addWeighted(cv_seg_image, 0.9, cv_image, 0.8, 0)
 
-  return cv_mask_image
+  return cv_mask_image,cv_seg_image
   
-  """
-  {{{
-  plt.figure(figsize=(15, 5))
-  grid_spec = gridspec.GridSpec(1, 4, width_ratios=[6, 6, 6, 1])
-
-  plt.subplot(grid_spec[0])
-  plt.imshow(image)
-  plt.axis('off')
-  plt.title('input image')
-
-  plt.subplot(grid_spec[1])
-  seg_image = label_to_color_image(seg_map).astype(np.uint8)
-  plt.imshow(seg_image)
-  plt.axis('off')
-  plt.title('segmentation map')
-
-  plt.subplot(grid_spec[2])
-  plt.imshow(image)
-  plt.imshow(seg_image, alpha=0.7)
-  plt.axis('off')
-  plt.title('segmentation overlay')
-
-  unique_labels = np.unique(seg_map)
-  ax = plt.subplot(grid_spec[3])
-  plt.imshow(
-      FULL_COLOR_MAP[unique_labels].astype(np.uint8), interpolation='nearest')
-  ax.yaxis.tick_right()
-  plt.yticks(range(len(unique_labels)), LABEL_NAMES[unique_labels])
-  plt.xticks([], [])
-  ax.tick_params(width=0.0)
-  plt.grid('off')
-  plt.show()
-  }}}
-  """
 class Segmentation(object):
     def __init__(self):
         self.image_sub = rospy.Subscriber("/usb_cam/image_raw", rosImage, self.imageCallback, queue_size=10)
-        self.image_pub = rospy.Publisher("/deeplab/image", rosImage, queue_size=10)
+        self.image_pub1 = rospy.Publisher("/deeplab/image", rosImage, queue_size=10)
+        self.image2_pub = rospy.Publisher("/deeplab/seg_image", rosImage, queue_size=10)
+        self.TESTimg = cv2.imread("/shared/TEST.png", 1)
 
     def imageCallback(self, image_msg):
         try:
             self.cv_Image = CvBridge().imgmsg_to_cv2(image_msg, "bgr8")
-            self.pil_Image = Image.fromarray(cv2.cvtColor(self.cv_Image, cv2.COLOR_BGR2RGB))
+            self.pil_Image = Image.fromarray(cv2.cvtColor(self.TESTimg, cv2.COLOR_BGR2RGB))
+            # self.pil_Image = Image.fromarray(cv2.cvtColor(self.cv_Image, cv2.COLOR_BGR2RGB))
             resized_im, seg_map = MODEL.run(self.pil_Image)
-            seg_image = vis_segmentation(resized_im, seg_map)
+            seg_image1,  seg_image2= vis_segmentation(resized_im, seg_map)
             
-            pub_image = CvBridge().cv2_to_imgmsg(seg_image, "bgr8")
-            self.image_pub.publish(pub_image)
+            pub_image1 = CvBridge().cv2_to_imgmsg(seg_image1, "bgr8")
+            self.image_pub1.publish(pub_image1)
 
+            pub_image2 = CvBridge().cv2_to_imgmsg(seg_image2, "bgr8")
+            self.image2_pub.publish(pub_image2)
         except CvBridgeError as e:
             print (e)
 
@@ -163,7 +135,7 @@ LABEL_NAMES = np.asarray([
 FULL_LABEL_MAP = np.arange(len(LABEL_NAMES)).reshape(len(LABEL_NAMES), 1)
 FULL_COLOR_MAP = label_to_color_image(FULL_LABEL_MAP)
 
-MODEL_NAME = 'deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz'
+MODEL_NAME = 'deeplabv3_xcep65_tomato_train.tar.gz'
 # MODEL_NAME = 'deeplabv3_mnv2_cityscapes_train_2018_02_05.tar.gz'
 download_path = os.path.join(sys.path[0], MODEL_NAME)
 MODEL = DeepLabModel(download_path)
